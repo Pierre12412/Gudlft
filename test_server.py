28b875c7be277682867a03f09fe3d03fb621bc2e
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-10-05 14:41:43 +0200

    tests

--- test_server.py
+++ test_server.py
@@ -12,3 +12,17 @@ def test_connexion_right():
     response = app.test_client().post('/showSummary',data=dict(email="admin@irontemple.com",))
     assert not "Sorry, that email was not found." in str(response.data)
     assert response.status_code == 200
+    assert 'Welcome, admin@irontemple.com' in str(response.data)
+
+def test_finished_or_not():
+    app.test_client().post('/showSummary',data=dict(email="admin@irontemple.com"))
+    response = app.test_client().get('/showSummary')
+    assert 'Competition is finished' or 'Hurry up' in str(response.data);
+
+def test_show_clubs():
+    response = app.test_client().post('/showSummary', data=dict(email="admin@irontemple.com"))
+    assert 'Clubs:' in str(response.data)
+
+def test_display():
+    response = app.test_client().get('/display')
+    assert 'Iron Temple' in str(response.data)
